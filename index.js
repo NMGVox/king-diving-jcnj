4c6913c658545f4f1a29d90c99e927df6275b3cf
Nijee Greene <nijeemgreene@gmail.com>
2026-06-01 16:03:24 -0400

    MAjor mobile screen optimization updates

---
+++ index.js
@@ -0,0 +1,15 @@
+function hideMenu(){
+    const sidebar = document.getElementById("sidebar");
+    console.log(sidebar.classList);
+    if (sidebar.classList.contains("show")) {
+        /*Hide Menu*/
+        document.querySelector('.closeMenu').src = "./img/bars-solid-full.svg";
+        sidebar.classList.remove("show");
+        return;
+    }
+    /*Show Menu*/
+    document.querySelector('.closeMenu').src = "./img/x-solid-full.svg";
+    sidebar.classList.add("show")
+    return;
+
+}
